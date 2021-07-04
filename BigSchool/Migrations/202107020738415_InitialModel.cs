b3583e82108bf832c207e19e841ab6a4a2e03769
vanthom <vthom123.cva@gmail.com>
2021-07-04 20:34:08 +0700

    Buổi 4

---
+++ BigSchool/Migrations/202107020738415_InitialModel.cs
@@ -0,0 +1,47 @@
+namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Courses", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.Courses", "LecturerId", "dbo.AspNetUsers");
+            DropIndex("dbo.Courses", new[] { "LecturerId" });
+            DropIndex("dbo.Courses", new[] { "CategoryId" });
+            DropTable("dbo.Categories");
+            DropTable("dbo.Courses");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LecturerId = c.String(nullable: false, maxLength: 128),
+                        Place = c.String(nullable: false, maxLength: 255),
+                        DateTime = c.DateTime(nullable: false),
+                        CategoryId = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.Courses", "CategoryId");
+            CreateIndex("dbo.Courses", "LecturerId");
+            AddForeignKey("dbo.Courses", "LecturerId", "dbo.AspNetUsers", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.Courses", "CategoryId", "dbo.Categories", "Id", cascadeDelete: true);
+        }
+    }
+}
